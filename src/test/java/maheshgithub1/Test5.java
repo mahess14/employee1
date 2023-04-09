c78295f31cb3ec722f1997122c73e8123ba2204e
mahess14 <gmksvec14@gmail.com>
2023-04-09 18:32:48 +0530

    committing test5

---
+++ src/test/java/maheshgithub1/Test5.java
@@ -0,0 +1,18 @@
+package maheshgithub1;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+
+		int x=50;
+		int y=600;
+		int r=700;
+		int o=800;
+		int z=x+y+r+o;
+		
+			System.out.println(z);
+		}
+	}
+
+
+
