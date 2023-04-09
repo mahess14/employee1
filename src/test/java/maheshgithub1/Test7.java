32800da2eb2c49099c147d12e7a04b26ef25198e
mahess14 <gmksvec14@gmail.com>
2023-04-09 18:53:04 +0530

    committing77

---
+++ src/test/java/maheshgithub1/Test7.java
@@ -0,0 +1,18 @@
+package maheshgithub1;
+
+public class Test7 {
+
+	public static void main(String[] args) {
+
+		int x=50;
+		int y=600;
+		int r=700;
+		int t=800;
+		int z=x+y+r+t;
+		
+			System.out.println(z);
+		}
+	}
+
+
+
