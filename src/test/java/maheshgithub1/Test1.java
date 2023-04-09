74b0280e669c3d44804960854c5c8c4e9eb53aaf
mahess14 <gmksvec14@gmail.com>
2023-04-09 18:20:59 +0530

    COMITTING TEST1

--- src/test/java/maheshgithub1/Test1.java
+++ src/test/java/maheshgithub1/Test1.java
@@ -3,7 +3,7 @@ package maheshgithub1;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("hi testing 1 test");
+		System.out.println("hi testing 1 Test");
 
 	}
 
