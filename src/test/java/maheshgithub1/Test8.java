8e3882adfc93e4e364dd9837fdfb01078c34be50
mahess14 <gmksvec14@gmail.com>
2023-04-09 19:38:30 +0530

    comtting88

---
+++ src/test/java/maheshgithub1/Test8.java
@@ -0,0 +1,19 @@
+package maheshgithub1;
+
+public class Test8 {
+
+	public static void main(String[] args) {
+
+		int x=50;
+		int y=600;
+		int r=700;
+		int t=800;
+		int s=900;
+		int z=x+y+r+t;
+		
+			System.out.println(z);
+		}
+	}
+
+
+
